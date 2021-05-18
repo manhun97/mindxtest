af8e05c91e0b504ac6eccaf84394d1c5cbce2b1f
Mạnh Hùng Nguyễn <x01643353354x@yahoo.com>
2021-05-18 13:22:37 +0700

    modified

--- B/index.js
+++ B/index.js
@@ -3,19 +3,28 @@ fetch(
 )
 	.then((res) => res.json())
 	.then((out) => {
-		let app = document.getElementById("app");
-		let res = out.results;
+		let result = out.results;
 		let i = 0;
-		res.forEach((item) => {
+		result.forEach((item) => {
 			i++;
 			let question = document.createElement("div");
-			answer = item.incorrect_answers.push(item.correct_answer);
+			let answer = item.incorrect_answers;
+			answer.push(item.correct_answer);
 			answer = answer.sort(() => Math.random() - 0.5);
 			question.innerHTML = `
-         <div>Question ${i}: ${item.question}</div>
-         <div>Answers: ${answer}</div>
-         `;
+      <div class="question">Question ${i}: ${item.question}</div>
+      <br>
+      <div>Answers:</div>
+      <br>
+      <div class="answer">A. ${answer[0]}</div>
+      <div class="answer">B. ${answer[1]}</div>
+      <div class="answer">C. ${answer[2]}</div>
+      <div class="answer">D. ${answer[3]}</div>
+      <br>
+      `;
 			app.appendChild(question);
 		});
+		console.log(answer);
+		console.log(result);
 	})
 	.catch((err) => console.error(err));
